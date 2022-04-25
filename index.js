87967c1fa2c721054458a9718632896dd5426b64
Kenneth <kenneth.l.thomsen@gmail.com>
2022-04-25 10:44:07 +0200

    part2

--- index.js
+++ index.js
@@ -6,20 +6,29 @@ canvas.height = 578
 
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+
+const grav = 0.2
 class Sprite {
     constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
+        this.height = 150
     }
 
     draw() {
         ctx.fillStyle = 'red'
-        ctx.fillRect(this.position.x, this.position.y, 50, 150);
+        ctx.fillRect(this.position.x, this.position.y, 50, this.height);
 }
 
     update() {
         this.draw()
-        this.position.y += 10
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height >= canvas.height) {
+            this.velocity.y = 0
+        } else{
+            this.velocity.y += grav
+        }
     }
 }
 
@@ -50,11 +59,19 @@ const enemy = new Sprite({
 
 function animate(){
     window.requestAnimationFrame(animate)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
 }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+    console.log(event.key);
+    switch (event.key) {
+        case 'd':  player.velocity.x = 1;
+}
+})
 
 console.log(player);
