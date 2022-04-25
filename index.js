e26aaab930d4a77be0775707c816236c99795f68
Kenneth <kenneth.l.thomsen@gmail.com>
2022-04-25 10:27:54 +0200

    Tekken Init

---
+++ index.js
@@ -0,0 +1,60 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 1024
+canvas.height = 578
+
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+    }
+
+    draw() {
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.position.x, this.position.y, 50, 150);
+}
+
+    update() {
+        this.draw()
+        this.position.y += 10
+    }
+}
+
+const player = new Sprite({
+    position: {
+    x: 0,
+    y: 0
+},
+    velocity: {
+    x: 0,
+    y: 0,
+
+    }
+})
+
+const enemy = new Sprite({ 
+    position : {
+    x: 500,
+    y: 100
+},
+    velocity: {
+    x: 0,
+    y: 0,
+
+    }
+})
+
+
+function animate(){
+    window.requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+}
+
+animate()
+
+console.log(player);
